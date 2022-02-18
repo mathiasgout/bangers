0d767fcaed6db06564b5e7bb473033d7c8a0ba65
Mathias Gout <mathias.gout@adopteunmec.com>
2022-02-18 16:19:40 +0100

    'if __name__..' added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src import twitter_bot
 
-user_id = 812926437032390656
-extractor = twitter_bot.ExtractTweets()
-extractor.insert_last_tweets_into_db(user_id=user_id)
-tweet_poster = twitter_bot.ReplyTweet()
-tweet_poster.post_tweet(user_id=user_id)
+if __name__ == "__main__":
+    user_id = 812926437032390656
+    extractor = twitter_bot.ExtractTweets()
+    extractor.insert_last_tweets_into_db(user_id=user_id)
+    tweet_poster = twitter_bot.ReplyTweet()
+    tweet_poster.post_tweet(user_id=user_id)
+
